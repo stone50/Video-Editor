3d0c9f72998883bb81d367c471f6b184001ec048
Stone Hobbs <stoneh826@gmail.com>
2023-05-03 18:00:56 -0500

    Updated gitignore and fixed cut_duplicate_frames

--- cut_duplicate_frames.py
+++ cut_duplicate_frames.py
@@ -4,6 +4,7 @@ import cv2
 from utils import format_time
 from image_similarity import get_image_similarity
 
+
 def cut_duplicate_frames(inputFileName: str, similarityThreshold: float = 1, printMessages: bool = True):
 
     # open the input video
@@ -33,40 +34,59 @@ def cut_duplicate_frames(inputFileName: str, similarityThreshold: float = 1, pri
     # process the input video
     success, frame = inputVideo.read()
     while success:
-        
+
         # print progress
         if printMessages:
-            print(f"\rVideo Time Elapsed: {format_time(int(inputVideo.get(cv2.CAP_PROP_POS_MSEC)))}\t( {inputVideo.get(cv2.CAP_PROP_POS_FRAMES) / INPUT_VIDEO_FRAME_COUNT:.2%} )", end="")
-        
+            print(
+                f"\rVideo Time Elapsed: {format_time(int(inputVideo.get(cv2.CAP_PROP_POS_MSEC)))}\t( {inputVideo.get(cv2.CAP_PROP_POS_FRAMES) / INPUT_VIDEO_FRAME_COUNT:.2%} )", end="")
+
         # copy the frame to the output video if it is different than the previous frame
         if get_image_similarity(frame, previousFrame) < similarityThreshold:
             outputVideo.write(frame)
             previousFrame = frame
             framesCopied += 1
-        
+
         # read the next frame
         success, frame = inputVideo.read()
 
     # print results
     if printMessages:
         print('\n')
-        print(f"New Video Length: {format_time(int(framesCopied * 1_000 / INPUT_VIDEO_FPS))}")
+        print(
+            f"New Video Length: {format_time(int(framesCopied * 1_000 / INPUT_VIDEO_FPS))}")
 
     # cleanup
     inputVideo.release()
     outputVideo.release()
-    
+
+
 if __name__ == "__main__":
-    
+
     # setup argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("fileName", type=str, help="name of the video file including the file extension")
-    parser.add_argument("--threshold", "-t", type=float, default=1, choices=range(0, 1), help="similarity threshold (1 = cut only exact duplicates, 0 = cut all frames)")
-    parser.add_argument("--no-messages", "-nm", dest="messages", action="store_false", help="do not print messages")
-    
+    parser.add_argument(
+        "fileName",
+        type=str,
+        help="name of the video file including the file extension"
+    )
+    parser.add_argument(
+        "--threshold",
+        "-t",
+        type=float,
+        default=1,
+        help="similarity threshold (1 = cut only exact duplicates, 0 = cut all frames)"
+    )
+    parser.add_argument(
+        "--no-messages",
+        "-nm",
+        dest="messages",
+        action="store_false",
+        help="do not print messages"
+    )
+
     # parse arguments
     args = parser.parse_args()
-    
+
     # cut duplicate frames
     cut_duplicate_frames(
         args.fileName,
